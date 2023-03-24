4e859fa0c59357847318542716c9dbfb8f209f56
207 <207@academicos.cesde.edu.co>
2023-03-24 08:37:36 -0500

    Se agrego un .config de la documentacion

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv'
+dotenv.config()
+
 import {API} from "./API.js"
-console.log(process.env)
+console.log(process.env.PORT)
 let servidor = new API
 
 //1. Despertar el servidor
